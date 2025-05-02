8270e96fd444d83259c3b3aadfd2de88da87bd39
Evan Lagan <evanlagan21@gmail.com>
2025-05-02 23:29:57 +0100

    Added CLI for adding blocks, validating and printing the chain. Next step is to add persistence.

--- main.go
+++ main.go
@@ -2,20 +2,55 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage:")
+		fmt.Println("	addblock \"some data\"")
+		fmt.Println("	printchain")
+		fmt.Println("	validate")
+		return
+	}
+
+	command := os.Args[1]
 	chain := NewBlockchain()
 
-	chain.AddBlock("First block after Genesis")
-	chain.AddBlock("Second block after Genesis")
+	switch command {
+	case "addblock":
+		if len(os.Args) < 3 {
+			fmt.Println("Missing data for new block")
+			return
+		}
+		data := strings.Join(os.Args[2:], " ")
+		chain.AddBlock(data)
 
-	for i, block := range chain.Blocks {
-		fmt.Printf("Block #%d\n", i)
-		fmt.Printf("Timestamp: %s\n", block.Timestamp.String())
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Previous Hash: %s\n", block.PrevHash)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Println()
+	case "printchain":
+		for i, block := range chain.Blocks {
+			fmt.Printf("\n Block #%d\n", i)
+			fmt.Printf("Timestamp:    %s\n", block.Timestamp)
+			fmt.Printf("Data:         %s\n", block.Data)
+			fmt.Printf("PrevHash:     %s\n", block.PrevHash)
+			fmt.Printf("Hash:         %s\n", block.Hash)
+			fmt.Printf("Nonce:        %d\n", block.Nonce)
+		}
+
+	case "validate":
+		valid := true
+		for i := 1; i < len(chain.Blocks); i++ {
+			if !isValidBlock(chain.Blocks[i], chain.Blocks[i-1]) {
+				fmt.Printf("Block #%d is invalid!\n", i)
+				valid = false
+			}
+		}
+		if valid {
+			fmt.Println("Blockchain is valid.")
+		}
+
+	default:
+		fmt.Println("Unknown command:", command)
 	}
+
 }
